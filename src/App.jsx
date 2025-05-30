1f89c55f8b943bf394ca0a3022b05c21f828b054
Alex Akins <akins@Alexs-Mac-mini.attlocal.net>
2025-05-29 20:25:39 -0400

    Website placeholder release

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx'
+import Revamping from './pages/Revamping.jsx'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Revamping />} />
       </Routes>
     </BrowserRouter>
   );
